6aa6f933898470f9f2615322d15c66bd235d9e5c
Alejandro Pulido <hapulido22@gmail.com>
2021-04-03 14:20:43 -0500

    primer commit

---
+++ js/portafolio.js
@@ -0,0 +1,84 @@
+$(document).ready(function() {
+
+
+    // EFECTOS DE TRANSICIONES AL CARGAR
+    $('.banner-titulo').each(function(index, elemento) {
+        $(this).css({
+            animationName: 'titulos-in',
+            animationDuration: '1s'
+        });
+    });
+
+
+    $('.btn-inicio').css({
+        animationName: 'titulos-in',
+        animationDuration: '1s'
+    });
+
+    /* Navegacion dinamica barra lateral */
+    $('.nav-div').hover(function() {
+        $(this).children('a').children('span').fadeIn();
+        $(this).css({
+            backgroundColor: '#5830FF',
+            transition: 'all 1s'
+        });
+    }, function() {
+        $(this).children('a').children('span').fadeOut();
+        $(this).css({
+            backgroundColor: 'transparent',
+            transition: 'all 1s'
+        });
+    });
+
+    /* Navigation burger onclick side navigation show */
+    $('.burger-container').on('click', function() {
+        $('.main-navigation').toggle('slow');
+
+        if ($('#myBtn').hasClass('change')) {
+            $('body').addClass('stop-scroll');
+        } else {
+            $('body').removeClass('stop-scroll');
+        }
+    });
+
+    var posicionPort = $('#top-pag-portafolio').offset().top;
+    var posicionItems = $('#portafolio-items').offset().top;
+
+    $(window).scroll(function() {
+        var posicionActual = $(window).scrollTop();
+        if (posicionActual > posicionPort + 400) {
+
+            $('.banner-titulo').css({
+                transform: 'translateX(100%)',
+                transition: '1s'
+            });
+            $('.btn-empezar').css({
+                left: '400px',
+                opacity: '0'
+            });
+        } else {
+            $('.banner-titulo').css({
+                transform: 'translateX(0%)',
+                transition: '1s'
+            });
+            $('.btn-empezar').css({
+                left: '0',
+                opacity: '1',
+                transition: 'all 0.8s ease-out'
+            });
+        }
+
+
+
+        if (posicionActual > posicionItems - 400) {
+
+            $('.item-porta').each(function(index, elemento) {
+                $(this).fadeIn(300 + (index * 200));
+            });
+        } else {
+            $('.item-porta').each(function(index, elemento) {
+                $(this).fadeOut(300 + (index * 200));
+            });
+        }
+    });
+});
